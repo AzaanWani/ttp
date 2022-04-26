f075fe1f66b85efbef6241c407a8fe7e3f27be18
Your Name <tantryriyaz87@gmail.com>
2022-04-26 13:49:56 +0530

    update

---
+++ i-art.php
@@ -0,0 +1,154 @@
+<?php
+  require_once('header.php');
+  $activePage = "Services";
+?>
+
+<section id="main-section" class="clearfix">
+	<div class="container pt-5">
+<!-- Body Section -->
+
+     
+<section id="about" class="about privacy-policy" >
+
+    <div class="container">
+        <div class="row" data-aos="fade-up">          
+
+            <div class="col-lg-12 col-md-12" >
+                <div class="about-content" data-aos="fade-left" >
+                    <h2 class="text-center">   I-ART  </h2>
+                    <p><h5><b>I-ART</b> allows you to skip making a trip to the clinic or a test centre just to do your Covid-19<b> Pre-Event Test (PET)</b> or<b> Pre-Departure Test (PDT)*</b>. For anyone who requires a Supervised Self-Administered Antigen Rapid Test (ART) for a variety of purposes, you can now do it via <b>I-ART</b>. 
+</h5></p>
+                    <p>
+                        
+                        <div class="table-responsive">
+                            <table class="table table-hover table-bordered">
+                                <div class="tbody">
+                                   
+                                    
+                                    <tr>    
+                                        <th class="text-center">What</th>
+                                        <td>Real-Time Remote Supervised Antigen Rapid Test (ART)</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="text-center">For</th>
+                                        <td>Pre-Event Test (PET)<br>
+                                             Pre-Departure Test (PDT)*</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="text-center">Where</th>
+                                        <td>Via Google Meets</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="text-center">Price</th>
+                                        <td>SPET: S$15.00 per test<br>
+                                             PDT*: S$25.00 per test (inclusive of Notarised Digital HealthCert)</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="text-center">How</th>
+                                        <td>Book Appointment Here -><br> <a href="book-i-art.php"><u> https://www.picktime.com/thetestingpro </u> </a></td>
+                                    </tr>
+                                    <tr>
+                                        <th class="text-center">Includes</th>
+                                        <td>Results via SMS (and updated on HealthHub / TraceTogether App)<br>
+                                             Notarized Digital Health Certificate (only where applicable)</td>                                        
+                                    </tr>
+                                    <tr>     
+                                        <th class="text-center">Indemnity</th>
+                                        <td colspan="2">TheTestingPro is only responsible for <u>supervising </u>the Self-Administered Antigen Rapid Test over video calling, 
+                                            and issuing the Notarized Digital Health Certificate of test results, where applicable. It is the client’s responsibility 
+                                            to ensure the testing window requirements of the specific travel/event and that the appointment can meet the required timeline,
+                                             as well as the validity of<u> Supervised Self-Administered ART</u>, before booking any of such test with us.</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>  
+                    </p>
+                </div>
+            </div>
+        
+        </div>
+        <div class="row" >
+            <div class="col-md-12" data-aos="fade-right">
+                <p>  <i><span class="text-danger">* </span>Only for countries that accept Supervised Self-Administered Antigen Rapid Test (ART) as a form of Pre-Departure Test (PDT)
+               </i> </p>
+                <p>
+                    <h5><b>About Test Results and Notarised Digital HealthCert</b></h5>
+                        <ul>
+                            <li>Test Result will be ready 1 hour from completion of test</li> &nbsp;
+                            <li>Test Result will be sent via SMS (where applicable)</li>&nbsp;
+                            <li>Notarized Digital HealthCert, where applicable, will be issued within 2 hours from receipt of the Online HealthCert Request (HCR) Form, and will be issued via email only </li>&nbsp;
+                             <li>Notarized Digital HealthCerts include the EU-DCC compatible QR code </li>
+                             
+                        </ul> 
+                </p>
+                       <h5 class="text-center"><b>TheTestingPro (in partnership with SM Haneefa Clinic) is licensed by the Ministry of Health (MOH) Singapore to conduct Real-Time Remote Supervised ART </b></h5>
+                              <p> <center><a href="https://www.moh.gov.sg/covid-19/quick-test-centres-(qtcs)"><u> https://www.moh.gov.sg/covid-19/quick-test-centres-(qtcs)</u> </a></center></p>
+            </div>
+            <div class="col-md-12" data-aos="fade-right">
+              <h4 class="text-center"> <span class="text-danger"><b>  
+                                        I-ART  <br>
+                                        Remote Supervised Self-Administered ART<br>
+                                        PET: S$15.00 per test <br>
+                                        PDT: $25.00 per test <br>
+                                        (all inclusive) 
+                </b> </span></h4>
+                 <h6 class="text-center"><b>You are required to have at least two (2)<a href="https://www.hsa.gov.sg/consumer-safety/articles/covid19_selftests"><u> HSA Approved </u></a>ART Kit </b></h6>
+                 <h6 class="text-center"> To book an I-ART appointment, please <a href="https://www.picktime.com/thetestingpro"><u> BOOK HERE</u> </a> </h6>
+                 <h6 class="text-center"><b> For any enquiries about I-ART, please email to <a href= "mailto:concierge@thetestingpro.com"> <u>concierge@thetestingpro.com</u></b></a></h6>
+                 <p>
+                     <h5><b>Important Notes, Requirements, Terms & Conditions</b></h5>
+                     <ul>
+                            <li>Operation Hours:<b> 9:00 AM – 9:00 PM </b></li><br>
+                            <li>You will require to have at least <b>two (2)</b> test kits that are <a href="https://www.hsa.gov.sg/consumer-safety/articles/covid19_selftests"><u>approved by HSA Singapore</u></a></li> <br>
+                            <li>You will require to have a dual-camera phone or tablet, which can show on screen the entire testing area and the individual. </li><br>
+                             <li>It is a requirement to show your IC or Passport (for Pre-departure travelers) for verification purpose during the test session. </li><br>
+                             <li> Log on at appointment time and follow the instructions given by our I-ART supervisor.</li><br>
+                             <li>Position your phone/tablet/laptop on a stable platform/fixture and ensure your screen is able capture a full view of both yourself and the test kit placed in front of you.</li><br>
+                             <li> Please switch off all phone/tablet notifications that will hinder the test recording</li><br>
+                             <li> Your test results will be sent to you via SMS/email from HPB within 2 hours after your test is done and it will also be updated on your TraceTogether / HealthHub (where applicable) as well. </li> <br>
+                                Note: Please contact our hotline (+65 8011 8728) if you did not receive your ART results
+                             
+                        </ul> 
+                 </p>
+                 <p> 
+                 <h5><b>I-ART session</b></h5>
+                 <ul>
+                             <li> Sanitise hands before the test commence.</li><br>
+                             <li> We will need to verify your test kits. Display both your ART kits to our I-ART Supervisor (ensure the ART batch/LOT number is shown to us as well).</li><br>
+                             <li> Follow the instructions given by our I-ART supervisor for the test.</li><br>
+                             <li> Wait 15 minutes for the ART results to be confirmed. Ensure camera is zoomed in on the ART kit that is placed on display throughout the entire session.</li><br>
+                             <li> Once 15 minutes is up, our I-ART supervisor will record your result and submit to MOH. </li><br>
+                              Note: Only in the situation when the test result is invalid, our I-ART supervisor will prompt you re-do the test with the second ART kit.
+                 </ul>     
+                 </p>
+            </div>
+            <div class="col-md-12" data-aos="fade-right">
+              <h4 class="text-center"> <span class="text-danger"><b>  
+                                        I-ART  <br>
+                                        Remote Supervised Self-Administered ART<br>
+                                        PET: S$15.00 per test <br>
+                                        PDT: $25.00 per test <br>
+                                        (all inclusive) 
+                </b> </span></h4>
+                 <h6 class="text-center"> <b>You are required to have at least two (2)<a href="https://www.hsa.gov.sg/consumer-safety/articles/covid19_selftests"><u> HSA Approved </u></a>ART Kit </b></h6>
+                 <h6 class="text-center"> To book an I-ART appointment, please <a href="https://www.picktime.com/thetestingpro"><u> BOOK HERE</u> </a> </h6>
+                 <h6 class="text-center"><b> For any enquiries about I-ART, please email to <a href= "mailto:concierge@thetestingpro.com"> <u>concierge@thetestingpro.com</u></b></a></h6>
+             </div>
+            
+
+            
+        </div>
+
+    </div>
+
+</section>
+<!-- End Body Section -->
+</div>
+</section> 
+</main><!-- End #main -->
+
+
+<?php
+require_once('footer.php');
+?>
+
